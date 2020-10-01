f199fb6025ee67b7688b1df6b071a1f06dcb539a
Nave Levi <navelevi@outlook.com>
2020-10-01 23:56:02 +0300

    shopping data fill

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ def fillData(elementPath,keys, timer=20):
     WebDriverWait(driver, timer).until(EC.element_to_be_clickable((By.XPATH, elementPath))).send_keys(keys)
 
 # Main--------------------------------------------------------------------------------------------------------------------
-driver = webdriver.Chrome(chromeDriverPath)
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("start-maximized")
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromeDriverPath )
 driver.get("https://www.wallashops.co.il/Login")
 
 # Logging in
@@ -44,12 +49,15 @@ driver.get(data['productUrl'])
 # time.sleep(5)
 clickXpath('//*[@id="radio_1249600"]') #לא רוצה להחזיר
 clickXpath('//*[@id="NewBuyBox"]/div/div[3]/div/input[1]') #לקנייה
-
 clickXpath('//*[@id="radioShipmentMethodSelector33554436"]',20)
+#todo: click on something
 driver.find_element_by_xpath('//*[@id="full_name"]').send_keys(data['name'])
 driver.find_element_by_xpath('//*[@id="phone"]').send_keys(data['phoneNumber'])
 driver.find_element_by_xpath('//*[@id="city"]').send_keys(data['city'])
-# driver.find_element_by_xpath('//*[@id="street"]').send_keys(data['street'])
-# driver.find_element_by_xpath('//*[@id="zipcode"]').send_keys(data['zipCode'])
-# driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['aptNumber'])
-# driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['homeNumber'])
+clickXpath("/html/body/ul[1]/li")
+driver.find_element_by_xpath('//*[@id="street"]').send_keys(data['street'])
+clickXpath("/html/body/ul[2]/li")
+driver.find_element_by_xpath('//*[@id="zipcode"]').send_keys(data['zipCode'])
+driver.find_element_by_xpath('//*[@id="house_no"]').send_keys(data['homeNumber'])
+driver.find_element_by_xpath('//*[@id="apartment_no"]').send_keys(data['aptNumber'])
+driver.find_element_by_xpath('//*[@id="floor_no"]').send_keys(data['floorNumber'])
