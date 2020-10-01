5d3dd04932d81ea117bf40e7c2f5163e2639bea2
Raz Partouche <rzpr32@gmail.com>
2020-10-01 23:39:17 +0300

    FillData def added

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ with open('creds.yaml', encoding="utf8") as file:
 def clickXpath(elementPath, timer=20):
     WebDriverWait(driver, timer).until(EC.element_to_be_clickable((By.XPATH, elementPath))).click()
 
+def fillData(elementPath,keys, timer=20):
+    WebDriverWait(driver, timer).until(EC.element_to_be_clickable((By.XPATH, elementPath))).send_keys(keys)
 
 # Main--------------------------------------------------------------------------------------------------------------------
 driver = webdriver.Chrome(chromeDriverPath)
@@ -32,7 +34,7 @@ try:
     driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(data['password'])
     print('filled in pass')
     clickXpath('//*[@id="btnSubmit"]', 20)
-    print('filled in btnSubmit')
+    print('filled in btnsubmit')
 except:
     print('exception 2')
 
@@ -47,7 +49,7 @@ clickXpath('//*[@id="radioShipmentMethodSelector33554436"]',20)
 driver.find_element_by_xpath('//*[@id="full_name"]').send_keys(data['name'])
 driver.find_element_by_xpath('//*[@id="phone"]').send_keys(data['phoneNumber'])
 driver.find_element_by_xpath('//*[@id="city"]').send_keys(data['city'])
-driver.find_element_by_xpath('//*[@id="street"]').send_keys(data['street'])
-driver.find_element_by_xpath('//*[@id="zipcode"]').send_keys(data['zipCode'])
-driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['aptNumber'])
-driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['homeNumber'])
+# driver.find_element_by_xpath('//*[@id="street"]').send_keys(data['street'])
+# driver.find_element_by_xpath('//*[@id="zipcode"]').send_keys(data['zipCode'])
+# driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['aptNumber'])
+# driver.find_element_by_xpath('//*[@id="txtUserName"]').send_keys(data['homeNumber'])
